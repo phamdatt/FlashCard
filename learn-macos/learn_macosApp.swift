d7467b3e0a1ff60f9a1e5bb1e3cebd44c08c6c61
tiendatt3009@gmail.com <tiendatt3009@gmail.com>
2026-01-29 12:41:28 +0700

    Initial Commit

---
+++ learn-macos/learn_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_macosApp.swift
+//  learn-macos
+//
+//  Created by Dat Pham on 29/1/26.
+//
+
+import SwiftUI
+
+@main
+struct learn_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learn-macosTests/learn_macosTests.swift
@@ -0,0 +1,17 @@
+//
+//  learn_macosTests.swift
+//  learn-macosTests
+//
+//  Created by Dat Pham on 29/1/26.
+//
+
+import Testing
+@testable import learn_macos
+
+struct learn_macosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
